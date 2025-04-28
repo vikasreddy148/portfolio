b943fb01b20f9ac1fdd016f7a542d515639fe2da
vikasreddy148 <kathulavikasr@gmail.com>
2025-04-28 15:58:56 +0530

    Updated the project with new changes

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,40 +1,22 @@
-
-import { Github, Instagram, Twitter, Linkedin,} from 'lucide-react';
+import { Github, Instagram, Twitter, Linkedin } from "lucide-react";
 
 const SocialLinks = () => {
   return (
     <div className="fixed left-10 bottom-0 hidden md:block">
       <div className="flex flex-col items-center gap-6">
-        <a 
-          href="https://github.com/vikasreddy148" 
-          target="_blank" 
+        <a
+          href="https://github.com/vikasreddy148"
+          target="_blank"
           rel="noopener noreferrer"
           className="social-link-hover"
           aria-label="GitHub"
         >
           <Github size={20} />
         </a>
-        <a 
-          href="https://instagram.com/vikas.redddy" 
-          target="_blank" 
-          rel="noopener noreferrer"
-          className="social-link-hover"
-          aria-label="Instagram"
-        >
-          <Instagram size={20} />
-        </a>
-        <a 
-          href="https://twitter.com/Vikas1005" 
-          target="_blank" 
-          rel="noopener noreferrer"
-          className="social-link-hover"
-          aria-label="Twitter"
-        >
-          <Twitter size={20} />
-        </a>
-        <a 
-          href="https://www.linkedin.com/in/k-vikas-reddy/" 
-          target="_blank" 
+
+        <a
+          href="https://www.linkedin.com/in/k-vikas-reddy/"
+          target="_blank"
           rel="noopener noreferrer"
           className="social-link-hover"
           aria-label="LinkedIn"
